9c0cfc50f60b67b601dc66ba99fafe5d8d17d60f
Saoud Haidar <saoud.haidar@cmeoffshore.com>
2024-03-19 23:07:52 +0200

    Removing Alamofire dependency and downgrading to swift tools 5.3

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.9
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -12,9 +12,6 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(name: "NeoNetworking", targets: ["NeoNetworking"]),
         .library(name: "NeoLifestyle", targets: ["NeoLifestyle"]),
-    ],
-    dependencies: [
-        .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.8.1")),
     ],
     targets: [
         .binaryTarget(
